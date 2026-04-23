1d5d3cd217497dda96bb86f956e4443041a6ac07
JinGyo,Jeong <35288034+Gyofy@users.noreply.github.com>
2026-04-23 22:19:05 +0900

    Add files via upload

---
+++ people_data.js
@@ -0,0 +1,36 @@
+// people_data.js
+
+// 박사후연구원
+const postDoctors = [
+  { name: 'Junghun Han', topic: 'Medical AI', image: 'assets/junghun.jpg' },
+  { name: 'Yusung Chu', topic: 'Medical AI', image: 'assets/yu-seong.jpg' },
+];
+
+// 박사과정
+const phdStudents = [
+  { name: 'Sena Lee', topic: 'Medical AI', image: 'assets/sena.jpg' },
+  { name: 'Jiseung Ryu', topic: 'Medical AI', image: 'assets/jiseung.jpg' },
+  { name: 'Yerin Lee', topic: 'Medical AI', image: 'assets/yerin.jpg' },
+  { name: 'Jin Gyo Jeong', topic: 'Medical AI', image: 'assets/jingyo.jpg' },
+  { name: 'Hyunyoung Kang', topic: 'Medical AI', image: 'assets/hyunyoung.jpg' },
+  { name: 'Geunhye Kim', topic: 'Medical AI', image: 'assets/geunhye.jpg' },
+  { name: 'Younghyun Park', topic: 'Medical AI', image: 'assets/younghyun.jpg' },
+  { name: 'Janghoon Ru', topic: 'Medical AI', image: 'assets/janghoon.jpg' },
+];
+
+// 석사과정
+const msStudents = [
+  { name: 'Hyun min Bae', topic: 'Medical AI', image: 'assets/hyunmin.jpg' },
+  { name: 'Hee Jae Lee', topic: 'Medical AI', image: 'assets/heejae.jpg' },
+  { name: 'Hyun Mun', topic: 'Medical AI', image: 'assets/hyun.jpg' },
+];
+
+// 졸업생
+const alumni = [
+  ['이현우 (Hyunwoo Lee)', 'M.S.', '2022.02', '삼성 메디슨'],
+  ['장승현 (Seunghyun Jang)', 'M.S.', '2023.02', '서울대학교 의과대학 박사과정 진학'],
+  ['강승영 (SeungYoung Kang)', 'M.S.', '2023.02', '오스템 임플란트'],
+  ['이지영 (Jiyoung Lee)', 'M.S.', '2021.02', '삼성 메디슨'],
+  ['안홍기', 'M.S', '2021.08', '이마고웍스(주)'],
+  ['서경덕 (Kyungdeok Seo)', 'M.S', '2024.02', '오스템 임플란트'],
+];
