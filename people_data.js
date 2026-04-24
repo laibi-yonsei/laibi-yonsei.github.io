7e23a625ee3c9fc5e966b31e37d7ec82dcfbedd6
JinGyo,Jeong <35288034+Gyofy@users.noreply.github.com>
2026-04-24 12:32:43 +0900

    Update people_data.js

--- people_data.js
+++ people_data.js
@@ -2,27 +2,27 @@
 
 // 박사후연구원
 const postDoctors = [
-  { name: 'Junghun Han', topic: 'Medical AI', image: 'assets/junghun.jpg' },
-  { name: 'Yusung Chu', topic: 'Medical AI', image: 'assets/yu-seong.jpg' },
+  { name: 'Junghun Han', topic: 'Medical AI', image: 'assets/people/junghun.jpg' },
+  { name: 'Yusung Chu', topic: 'Medical AI', image: 'assets/people/yu-seong.jpg' },
 ];
 
 // 박사과정
 const phdStudents = [
-  { name: 'Sena Lee', topic: 'Medical AI', image: 'assets/sena.jpg' },
-  { name: 'Jiseung Ryu', topic: 'Medical AI', image: 'assets/jiseung.jpg' },
-  { name: 'Yerin Lee', topic: 'Medical AI', image: 'assets/yerin.jpg' },
-  { name: 'Jin Gyo Jeong', topic: 'Medical AI', image: 'assets/jingyo.jpg' },
-  { name: 'Hyunyoung Kang', topic: 'Medical AI', image: 'assets/hyunyoung.jpg' },
-  { name: 'Geunhye Kim', topic: 'Medical AI', image: 'assets/geunhye.jpg' },
-  { name: 'Younghyun Park', topic: 'Medical AI', image: 'assets/younghyun.jpg' },
-  { name: 'Janghoon Ru', topic: 'Medical AI', image: 'assets/janghoon.jpg' },
+  { name: 'Sena Lee', topic: 'Medical AI', image: 'assets/people/sena.jpg' },
+  { name: 'Jiseung Ryu', topic: 'Medical AI', image: 'assets/people/jiseung.jpg' },
+  { name: 'Yerin Lee', topic: 'Medical AI', image: 'assets/people/yerin.jpg' },
+  { name: 'Jin Gyo Jeong', topic: 'Medical AI', image: 'assets/people/jingyo.jpg' },
+  { name: 'Hyunyoung Kang', topic: 'Medical AI', image: 'assets/people/hyunyoung.jpg' },
+  { name: 'Geunhye Kim', topic: 'Medical AI', image: 'assets/people/geunhye.jpg' },
+  { name: 'Younghyun Park', topic: 'Medical AI', image: 'assets/people/younghyun.jpg' },
+  { name: 'Janghoon Ru', topic: 'Medical AI', image: 'assets/people/janghoon.jpg' },
 ];
 
 // 석사과정
 const msStudents = [
-  { name: 'Hyun min Bae', topic: 'Medical AI', image: 'assets/hyunmin.jpg' },
-  { name: 'Hee Jae Lee', topic: 'Medical AI', image: 'assets/heejae.jpg' },
-  { name: 'Hyun Mun', topic: 'Medical AI', image: 'assets/hyun.jpg' },
+  { name: 'Hyun min Bae', topic: 'Medical AI', image: 'assets/people/hyunmin.jpg' },
+  { name: 'Hee Jae Lee', topic: 'Medical AI', image: 'assets/people/heejae.jpg' },
+  { name: 'Hyun Mun', topic: 'Medical AI', image: 'assets/people/hyun.jpg' },
 ];
 
 // 졸업생
