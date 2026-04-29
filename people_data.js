0c5d0b217e9abf5af1562675f23cfc6b81899507
JinGyo,Jeong <35288034+Gyofy@users.noreply.github.com>
2026-04-29 16:03:55 +0900

    Add files via upload

--- people_data.js
+++ people_data.js
@@ -2,27 +2,43 @@
 
 // 박사후연구원
 const postDoctors = [
-  { name: 'Junghun Han', topic: 'Medical AI', email:'cheque@yonsei.ac.kr', image: 'assets/people/junghun.jpg' },
-  { name: 'Yusung Chu', topic: 'Medical AI', email:'dbtjd9968@yonsei.ac.kr', image: 'assets/people/yu-seong.jpg' },
+  { name: 'Junghun Han', topic: 'Medical AI', image: 'assets/people/junghun.jpg' },
+  { name: 'Yusung Chu', topic: 'Medical AI', image: 'assets/people/yu-seong.jpg' },
 ];
 
 // 박사과정
 const phdStudents = [
-  { name: 'Sena Lee', topic: 'Medical AI', email:'bluesky9707@naver.com', image: 'assets/people/sena.jpg' },
-  { name: 'Jiseung Ryu', topic: 'Medical AI', email:'ryujissss@yonsei.ac.kr', image: 'assets/people/jiseung.jpg' },
-  { name: 'Yerin Lee', topic: 'Medical AI', email:'leey01@yonsei.ac.kr', image: 'assets/people/yerin.jpg' },
-  { name: 'Jin Gyo Jeong', topic: 'Medical AI', email:'wlsry4008@yonsei.ac.kr', image: 'assets/people/jingyo.jpg' },
-  { name: 'Hyunyoung Kang', topic: 'Medical AI', email:'sonya23@yonsei.ac.kr', image: 'assets/people/hyunyoung.jpg' },
-  { name: 'Geunhye Kim', topic: 'Medical AI', email:'kimgeunhye99@gmail.com', image: 'assets/people/geunhye.jpg' },
-  { name: 'Younghyun Park', topic: 'Medical AI', email:'pyh2908@yonsei.ac.kr', image: 'assets/people/younghyun.jpg' },
-  { name: 'Janghoon Ru', topic: 'Medical AI', email:'octrjh@yonsei.ac.kr', image: 'assets/people/janghoon.jpg' },
+  { name: 'Sena Lee', topic: 'Medical AI', image: 'assets/people/sena.jpg' },
+  { name: 'Jiseung Ryu', topic: 'Medical AI', image: 'assets/people/jiseung.jpg' },
+  { name: 'Yerin Lee', topic: 'Medical AI', image: 'assets/people/yerin.jpg' },
+  { name: 'Jin Gyo Jeong', topic: 'Medical AI', image: 'assets/people/jingyo.jpg' },
+  { name: 'Hyunyoung Kang', topic: 'Medical AI', image: 'assets/people/hyunyoung.jpg' },
+  { name: 'Geunhye Kim', topic: 'Medical AI', image: 'assets/people/geunhye.jpg' },
+  { name: 'Younghyun Park', topic: 'Medical AI', image: 'assets/people/younghyun.jpg' },
+  { name: 'Janghoon Ru', topic: 'Medical AI', image: 'assets/people/janghoon.jpg' },
 ];
 
 // 석사과정
 const msStudents = [
-  { name: 'Hyun min Bae', topic: 'Medical AI', email:'hmbae0405@gmail.com', image: 'assets/people/hyunmin.jpg' },
-  { name: 'Hee Jae Lee', topic: 'Medical AI', email:'suiginto765@gmail.com', image: 'assets/people/heejae.jpg' },
-  { name: 'Hyun Mun', topic: 'Medical AI', email:'mun_hyun@yonsei.ac.kr', image: 'assets/people/hyun.jpg' },
+  { name: 'Hyun min Bae', topic: 'Medical AI', image: 'assets/people/hyunmin.jpg' },
+  { name: 'Hee Jae Lee', topic: 'Medical AI', image: 'assets/people/heejae.jpg' },
+  { name: 'Hyun Mun', topic: 'Medical AI', image: 'assets/people/hyun.jpg' },
+];
+
+// 협력 교수
+const collaborators = [
+  { name: 'Prof. Byungho Oh', department: 'Dept. of Dermatology, Yonsei University College of Medicine', email: 'obh505@gmail.com', image: 'assets/people/collab-byungho-oh.png' },
+  { name: 'Prof. Youngjun Seo', department: 'Dept. of Otorhinolaryngology, Yonsei University Wonju College of Medicine', email: 'okas2000@yonsei.ac.kr', image: 'assets/people/collab-youngjun-seo.png' },
+  { name: 'Prof. Moonyoung Kim', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: 'drkimmy@yonsei.ac.kr', image: 'assets/people/collab-moonyoung-kim.png' },
+  { name: 'Prof. Sooyoung Kim', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: 'breeze1212@yonsei.ac.kr', image: 'assets/people/collab-sooyoung-kim.png' },
+  { name: 'Prof. Sanhee Lee', department: 'Dept. of Obstetrics & Gynecology, Yonsei University Wonju College of Medicine', email: 'sanaram@yonsei.ac.kr', image: 'assets/people/collab-sanhee-lee.jpg' },
+  { name: 'Prof. Byungkeun Han', department: 'Dept. of Nephrology, Yonsei University Wonju College of Medicine', email: 'neptune@yonsei.ac.kr', image: 'assets/people/collab-byungkeun-han.jpg' },
+  { name: 'Prof. Youngjun Park', department: 'Dept. of Cardiology, Yonsei University Wonju College of Medicine', email: 'pyj@yonsei.ac.kr', image: 'assets/people/collab-youngjun-park.jpg' },
+  { name: 'Prof. Jinho Koh', department: 'Dept. of Convergence Medicine, Yonsei University Wonju College of Medicine', email: 'jinhokoh@yonsei.ac.kr', image: 'assets/people/collab-jinho-koh.png' },
+  { name: 'Prof. Jiye Kim', department: 'Dept. of Plastic Surgery, Yonsei University Wonju College of Medicine', email: 'gen80@yonsei.ac.kr', image: 'assets/people/collab-jiye-kim.jpg' },
+  { name: 'Prof. Seulgi Han', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: 'lolhsk@yonsei.ac.kr', image: 'assets/people/collab-seulgi-han.jpg' },
+  { name: 'Prof. Juryung Chung', department: 'Dept. of Orthodontics, Yonsei University College of Dentistry', email: 'CRCHUNG@yuhs.ac', image: 'assets/people/collab-juryung-chung.png' },
+  { name: 'Prof. Minseop Eom', department: 'Dept. of Pathology, Yonsei University Wonju College of Medicine', email: 'eomm@yonsei.ac.kr', image: 'assets/people/collab-minseop-eom.jpg' },
 ];
 
 // 졸업생
