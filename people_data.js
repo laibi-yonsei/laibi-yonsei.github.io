dc480c82bc4f86129c635db66ce3fa8190f7dd9c
JinGyo,Jeong <35288034+Gyofy@users.noreply.github.com>
2026-04-29 16:44:49 +0900

    Add files via upload

--- people_data.js
+++ people_data.js
@@ -2,7 +2,7 @@
 
 // 박사후연구원
 const postDoctors = [
-  { name: 'Junghun Han', topic: 'Multimodal AI & Image Generation in Medical domain', image: 'assets/people/junghun.jpg' },
+  { name: 'Junghun Han', topic: 'Medical AI', image: 'assets/people/junghun.jpg' },
   { name: 'Yusung Chu', topic: 'Medical AI', image: 'assets/people/yu-seong.jpg' },
 ];
 
@@ -33,11 +33,17 @@ const collaborators = [
   { name: 'Prof. Sooyoung Kim', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: 'breeze1212@yonsei.ac.kr', image: 'assets/people/collab-sooyoung-kim.png' },
   { name: 'Prof. Sanhee Lee', department: 'Dept. of Obstetrics & Gynecology, Yonsei University Wonju College of Medicine', email: 'sanaram@yonsei.ac.kr', image: 'assets/people/collab-sanhee-lee.jpg' },
   { name: 'Prof. Byungkeun Han', department: 'Dept. of Nephrology, Yonsei University Wonju College of Medicine', email: 'neptune@yonsei.ac.kr', image: 'assets/people/collab-byungkeun-han.jpg' },
+  { name: 'Prof. Youngjun Park', department: 'Dept. of Cardiology, Yonsei University Wonju College of Medicine', email: 'pyj@yonsei.ac.kr', image: 'assets/people/collab-youngjun-park.jpg' },
   { name: 'Prof. Jinho Koh', department: 'Dept. of Convergence Medicine, Yonsei University Wonju College of Medicine', email: 'jinhokoh@yonsei.ac.kr', image: 'assets/people/collab-jinho-koh.png' },
   { name: 'Prof. Jiye Kim', department: 'Dept. of Plastic Surgery, Yonsei University Wonju College of Medicine', email: 'gen80@yonsei.ac.kr', image: 'assets/people/collab-jiye-kim.jpg' },
   { name: 'Prof. Seulgi Han', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: 'lolhsk@yonsei.ac.kr', image: 'assets/people/collab-seulgi-han.jpg' },
   { name: 'Prof. Juryung Chung', department: 'Dept. of Orthodontics, Yonsei University College of Dentistry', email: 'CRCHUNG@yuhs.ac', image: 'assets/people/collab-juryung-chung.png' },
   { name: 'Prof. Minseop Eom', department: 'Dept. of Pathology, Yonsei University Wonju College of Medicine', email: 'eomm@yonsei.ac.kr', image: 'assets/people/collab-minseop-eom.jpg' },
+  { name: 'Prof. Hyunjeong Jeong', department: 'Dept. of Gastroenterology, Yonsei University Wonju College of Medicine', email: '', image: 'assets/people/collab-hyunjeong-jeong.jpg' },
+  { name: 'Prof. Taesik Lee', department: 'Dept. of Family Medicine, Yonsei University Wonju College of Medicine', email: '', image: 'assets/people/collab-taesik-lee.jpg' },
+  { name: 'Prof. Yongseong Cha', department: 'Dept. of Emergency Medicine, Yonsei University Wonju College of Medicine', email: '', image: 'assets/people/collab-yongseong-cha.jpg' },
+  { name: 'Prof. Kyojin Ahn', department: 'Dept. of Emergency Medicine, Yonsei University Wonju College of Medicine', email: '', image: 'assets/people/collab-kyojin-ahn.jpg' },
+  { name: 'Prof. Jaehyun Park', department: 'Dept. of Thyroid & Endocrine Surgery, Yonsei University Wonju College of Medicine', email: '', image: 'assets/people/collab-jaehyun-park.jpg' },
 ];
 
 // 졸업생
